f243a5564a5b2c4c2a827a9c24107bbcfcee03a1
Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>
2025-11-05 12:53:20 +0300

    Update manual
    Signed-off-by: Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>

--- docs/5g/manuals/body/js/136_2.0_jpn_15145880_2.0.js
+++ docs/5g/manuals/body/js/136_2.0_jpn_15145880_2.0.js
@@ -3,7 +3,7 @@ write("<div id=\"group2\"  name=\"PrtGrpId\" class=\"drag\" style=\"position:rel
 write("<div id=\"rect2\" name=\"PrtRectId\"class=\"drag\" strokecolor=\"white\" style=\"width:475.00px;height:319.74px;\">");
 write("<img id=\"img2\" name=\"PrtImgId\" src=\"../img/136_2.0_jpn_15145880_2.0.png\" border=\"1\" class=\"drag\" style=\"position:absolute;left:0px;top:0px;width:475.00px;height:319.74px;border-color:#ffffff\">");
 write("</div>");
-write("<p id=\"p2_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:17.389070px;top:206.644179px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<p id=\"p2_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:0px;top:206.644179px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
 write("<nobr>Увеличенный вид</nobr>");
 write("</p>");
 write("<p id=\"p2_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:298.081893px;top:227.392843px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
--- docs/5g/manuals/body/js/18_3.0_jpn_15145845_3.0.js
+++ docs/5g/manuals/body/js/18_3.0_jpn_15145845_3.0.js
@@ -4,7 +4,7 @@ write("<div id=\"rect100\" name=\"PrtRectId\"class=\"drag\" strokecolor=\"white\
 write("<img id=\"img100\" name=\"PrtImgId\" src=\"../img/18_3.0_jpn_15145845_3.0.png\" border=\"1\" class=\"drag\" style=\"position:absolute;left:0px;top:0px;width:475.00px;height:405.83px;border-color:#ffffff\">");
 write("</div>");
 write("<p id=\"p100_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:97.638266px;top:365.045105px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>Номер кадра</nobr>");
+write("<nobr>Номер модели и рамы</nobr>");
 write("</p>");
 write("</div>");
 }
