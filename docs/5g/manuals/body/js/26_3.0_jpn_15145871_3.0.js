2ecc24172fad1c0d61d5d5a9f9243d9f09c36a0a
Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>
2025-11-09 21:21:49 +0300

    Convert html to md
    Signed-off-by: Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>

--- docs/5g/manuals/body/js/26_3.0_jpn_15145871_3.0.js
+++ docs/5g/manuals/body/js/26_3.0_jpn_15145871_3.0.js
@@ -3,17 +3,17 @@ write("<div id=\"group1\"  name=\"PrtGrpId\" class=\"drag\" style=\"position:rel
 write("<div id=\"rect1\" name=\"PrtRectId\"class=\"drag\" strokecolor=\"white\" style=\"width:475.00px;height:868.53px;\">");
 write("<img id=\"img1\" name=\"PrtImgId\" src=\"../img/26_3.0_jpn_15145871_3.0.png\" border=\"1\" class=\"drag\" style=\"position:absolute;left:0px;top:0px;width:475.00px;height:868.53px;border-color:#ffffff\">");
 write("</div>");
-write("<p id=\"p1_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:48.412830px;top:381.029396px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr> Структура до нагрева</nobr>");
+write("<p id=\"p1_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:40px;top:360px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>Структура до нагрева</nobr>");
 write("</p>");
-write("<p id=\"p1_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:48.412830px;top:639.617500px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>Ткань после нагрева</nobr>");
+write("<p id=\"p1_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:40px;top:620px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>Структура после нагрева</nobr>");
 write("</p>");
-write("<p id=\"p1_3\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:83.601441px;top:468.183983px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>Подробно</nobr>");
+write("<p id=\"p1_3\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:40px;top:390px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>Мелкозернистая</nobr>");
 write("</p>");
-write("<p id=\"p1_4\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:94.326318px;top:729.832084px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>Грубый</nobr>");
+write("<p id=\"p1_4\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:40px;top:650px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>Крупнозернистая</nobr>");
 write("</p>");
 write("</div>");
 }
