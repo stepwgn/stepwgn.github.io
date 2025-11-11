71b51d0d2f1825c41ff72f66bb6e8db485b3dcae
Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>
2025-11-11 18:29:20 +0300

    Convert html to md
    Signed-off-by: Zakhar Petrov <46595458+zakhar-petrov@users.noreply.github.com>

--- docs/5g/manuals/body/js/130_2.0_jpn_15145877_2.0.js
+++ docs/5g/manuals/body/js/130_2.0_jpn_15145877_2.0.js
@@ -3,11 +3,11 @@ write("<div id=\"group3\"  name=\"PrtGrpId\" class=\"drag\" style=\"position:rel
 write("<div id=\"rect3\" name=\"PrtRectId\"class=\"drag\" strokecolor=\"white\" style=\"width:475.00px;height:637.94px;\">");
 write("<img id=\"img3\" name=\"PrtImgId\" src=\"../img/130_2.0_jpn_15145877_2.0.png\" border=\"1\" class=\"drag\" style=\"position:absolute;left:0px;top:0px;width:475.00px;height:637.94px;border-color:#ffffff\">");
 write("</div>");
-write("<p id=\"p3_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:328.986433px;top:152.729959px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>5.0mm</nobr>");
+write("<p id=\"p3_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:320px;top:152.729959px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>5.0 мм</nobr>");
 write("</p>");
-write("<p id=\"p3_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:358.106723px;top:581.865200px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
-write("<nobr>10mm</nobr>");
+write("<p id=\"p3_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:350px;top:581.865200px;font-weight:normal;font-size:11.2pt;color:black;\" prt_font_size=\"12.3pt\" disp_font_size=\"11.2pt\">");
+write("<nobr>10 мм</nobr>");
 write("</p>");
 write("</div>");
 }
